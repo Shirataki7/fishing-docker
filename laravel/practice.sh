c05163ee87e25c1766e6789900767dc294df67c0
nanami <88naechan88@gmail.com>
2020-05-12 17:05:44 +0900

    edit_file

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
+
 DOCKER_TAG="$1"
 
 AWSID=$(aws sts get-caller-identity)
 
-LOGIN=$(aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo)
+$(aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo)
 
-${LOGIN}
 
 docker tag $DOCKER_TAG:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/$DOCKER_TAG:latest
 
