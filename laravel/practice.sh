1fc31a933fd0074881fd8996e054c29364c1ee8a
nanami <88naechan88@gmail.com>
2020-05-13 21:55:24 +0900

    ECS

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-DOCKER_TAG="$1"
+AWSID=$(aws sts get-caller-identity | jq -r  .Account)
 
-AWSID=$(aws sts get-caller-identity)
+aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo
 
-$(aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo)
+docker tag feature/tag:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
 
+docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
 
-docker tag $DOCKER_TAG:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/$DOCKER_TAG:latest
-
-docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/$DOCKER_TAG:latest
+ecs-cli compose --file ../ecs/staging/ecs-compose.yml --ecs-params ../ecs/staging/ecs-params.yml up --create-log-groups
