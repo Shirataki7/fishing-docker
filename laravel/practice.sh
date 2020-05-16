8db210d8c62074fb85bcba460d239a46f1a8e07b
nanami <88naechan88@gmail.com>
2020-05-16 16:00:20 +0900

    edit_file

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -8,4 +8,4 @@ docker tag feature/tag:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practi
 
 docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
 
-ecs-cli compose --file ../ecs/staging/ecs-compose.yml --ecs-params ../ecs/staging/ecs-params.yml up --create-log-groups
+ecs-cli compose --project-name fishing --file ../ecs/staging/ecs-compose.yml --ecs-params ../ecs/staging/ecs-params.yml service up --create-log-groups
