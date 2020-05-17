1acbce1b3ea650dff9c4da71758f92e0f23c04c8
nanami <88naechan88@gmail.com>
2020-05-17 22:00:21 +0900

    add/build_file

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -4,8 +4,7 @@ AWSID=$(aws sts get-caller-identity | jq -r  .Account)
 
 aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo
 
-docker tag feature/tag:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
+docker tag practice-repo:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
 
-docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
+ecs-cli push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest 
 
-ecs-cli compose --project-name fishing --file ../ecs/staging/ecs-compose.yml --ecs-params ../ecs/staging/ecs-params.yml service up --create-log-groups
