e4381c9c240f50bcd1e8ab953d2b565aaba67a58
nanami <88naechan88@gmail.com>
2020-05-19 22:27:06 +0900

    edit/file

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -4,7 +4,12 @@ AWSID=$(aws sts get-caller-identity | jq -r  .Account)
 
 aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo
 
-docker tag practice-repo:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest
+#docker build -t fishing:latest -f ../infrastructure/docker/build/Dockerfile .
+#docker tag fishing:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+ecs-cli push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+ecs-cli compose --project-name laravel --file ../ecs/production/app/ecs-compose.yml --ecs-params ../ecs/production/app/ecs-params.yml service up --create-log-groups
 
-ecs-cli push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo:latest 
-
+docker build -t fishing:nginx -f ../infrastructure/docker/build/nginx/Dockerfile .
+docker tag fishing:nginx $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+ecs-cli push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+ecs-cli compose --project-name nginx --file ../ecs/production/nginx/ecs-compose.yml --ecs-params ../ecs/production/nginx/ecs-params.yml service up --create-log-groups
