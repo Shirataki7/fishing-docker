6260cfc6f23508867444113d4c25651eb40d9776
nanami <88naechan88@gmail.com>
2020-05-22 23:41:59 +0900

    edit_file

--- laravel/practice.sh
+++ laravel/practice.sh
@@ -7,11 +7,11 @@ aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker logi
 #buildはfishing-dockerディレクトリで
 
 #docker build -t fishing:latest -f ./infrastructure/docker/build/Dockerfile .
-#docker tag fishing:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
-#docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+docker tag fishing:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
 #ecs-cli compose --project-name app --file ../ecs/production/app/ecs-compose.yml --ecs-params ../ecs/production/app/ecs-params.yml service up --create-log-groups
 
 #docker build -t fishing:nginx -f ./infrastructure/docker/build/nginx/Dockerfile .
-docker tag fishing:nginx $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
-docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+#docker tag fishing:nginx $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+#docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
 #ecs-cli compose --project-name nginx --file ../ecs/production/nginx/ecs-compose.yml --ecs-params ../ecs/production/nginx/ecs-params.yml service up --create-log-groups
