aef0f74d326790283848cb6584e5c413ff5fcee8
nanami <88naechan88@gmail.com>
2020-05-28 22:38:32 +0900

    edit/.sh_file

---
+++ laravel/aws.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+AWSID=$(aws sts get-caller-identity | jq -r  .Account)
+
+aws ecr get-login-password --region ap-northeast-1 --profile user1 | docker login --username AWS --password-stdin 872475949043.dkr.ecr.ap-northeast-1.amazonaws.com/practice-repo
+
+#build、タグ付け、push
+cd ../fishing-docker
+docker build -t fishing:latest -f ./infrastructure/docker/build/Dockerfile .
+docker tag fishing:latest $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:latest
+
+docker build -t fishing:nginx -f ./infrastructure/docker/build/nginx/Dockerfile .
+docker tag fishing:nginx $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+docker push $AWSID.dkr.ecr.ap-northeast-1.amazonaws.com/fishing:nginx
+
+#サービスとタスクの作成起動
+cd laravel
+ecs-cli compose --project-name app --file ../ecs/production/app/ecs-compose.yml --ecs-params ../ecs/production/app/ecs-params.yml service up --target-group-arn arn:aws:elasticloadbalancing:ap-northeast-1:$AWSID:targetgroup/fishing-target-group/2b4999b192b9099c  --container-name nginx --container-port 80 --health-check-grace-period 120 --create-log-groups
