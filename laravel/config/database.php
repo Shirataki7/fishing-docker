db288a94959365295f98bdcceeb8c530a621078e
nanami <88naechan88@gmail.com>
2020-05-30 21:37:58 +0900

    edit/config.yml/migrate

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -69,7 +69,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'circle_test'),
-            'username' => env('DB_USERNAME', 'ubuntu'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
