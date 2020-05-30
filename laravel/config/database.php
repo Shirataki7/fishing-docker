012b87ceebc4170474984e42f3a77d86cdf4659e
nanami <88naechan88@gmail.com>
2020-05-30 18:42:14 +0900

    edit/config.yml/migrate

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -66,7 +66,7 @@ return [
         'mysql_testing' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'fishing_testing'),
             'username' => env('DB_USERNAME', 'root'),
