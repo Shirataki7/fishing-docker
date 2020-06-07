3ced0c4ddb4831401cf944589afee2b9a28a22ec
nanami <88naechan88@gmail.com>
2020-06-07 21:35:37 +0900

    edit/file

--- ecs.tf
+++ ecs.tf
@@ -3,10 +3,11 @@ resource "aws_ecs_cluster" "tsurins_cluster" {
 }
 
 resource "aws_launch_configuration" "tsuris_ecs_launch_config" {
-  name            = "tsurins_config"
-  image_id        = "ami-0f310fced6141e627"
-  instance_type   = "t3a.micro"
-  security_groups = [aws_security_group.tsurins_security_group.id]
+  name                 = "tsurins_config"
+  iam_instance_profile = "ecsInstanceRole"
+  image_id             = "ami-0f310fced6141e627"
+  instance_type        = "t3a.micro"
+  security_groups      = [aws_security_group.tsurins_security_group.id]
 }
 
 resource "aws_autoscaling_group" "tsurins_asg" {
