42d720a8f6281d2c36e0d78416dffc5bf758d2d5
nanami <88naechan88@gmail.com>
2020-06-07 18:32:34 +0900

    edit/ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -9,16 +9,22 @@ resource "aws_launch_configuration" "tsuris_ecs_launch_config" {
   security_groups = [aws_security_group.tsurins_security_group.id]
 }
 
-# resource "aws_autoscaling_group" "tsurins_asg" {
-#   name                 = "tsurins_autoscaling_group"
-#   vpc_zone_identifier  = [aws_subnet.tsurins_subnet_1a.id]
-#   launch_configuration = aws_launch_configuration.tsuris_ecs_launch_config.name
+resource "aws_autoscaling_group" "tsurins_asg" {
+  name                 = "tsurins_autoscaling_group"
+  vpc_zone_identifier  = [aws_subnet.tsurins_subnet_1a.id]
+  launch_configuration = aws_launch_configuration.tsuris_ecs_launch_config.name
 
-#   desired_capacity  = 1
-#   min_size          = 0
-#   max_size          = 1
-#   health_check_type = "EC2"
-# }
+  desired_capacity  = 1
+  min_size          = 0
+  max_size          = 1
+  health_check_type = "EC2"
+
+  tag {
+    key                 = "ClusterName"
+    value               = aws_ecs_cluster.tsurins_cluster.name
+    propagate_at_launch = true
+  }
+}
 
 resource "aws_appautoscaling_target" "tsurins_autoscaling_target" {
   max_capacity       = 1
