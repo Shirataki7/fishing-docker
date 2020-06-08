5037a9757a354b96ab1adb9e174ed341609048ec
nanami <88naechan88@gmail.com>
2020-06-08 14:49:03 +0900

    edit/db_instance

--- rds.tf
+++ rds.tf
@@ -7,19 +7,20 @@ resource "aws_db_subnet_group" "tsurins_db_subnets" {
 }
 
 resource "aws_db_instance" "tsurins_db" {
-  identifier           = "tsurins-production"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_subnet_group_name = aws_db_subnet_group.tsurins_db_subnets.name
-  parameter_group_name = "default.mysql5.7"
-  name                 = "tsurins_production"
-  port                 = 3306
-  username             = data.aws_ssm_parameter.tsurins_db_username.id
-  password             = data.aws_ssm_parameter.tsurins_db_password.id
-  availability_zone    = "ap-northeast-1a"
+  identifier             = "tsurins-production"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  db_subnet_group_name   = aws_db_subnet_group.tsurins_db_subnets.name
+  vpc_security_group_ids = [aws_security_group.tsurins_security_group.id]
+  parameter_group_name   = "default.mysql5.7"
+  name                   = "tsurins_production"
+  port                   = 3306
+  username               = data.aws_ssm_parameter.tsurins_db_username.id
+  password               = data.aws_ssm_parameter.tsurins_db_password.id
+  availability_zone      = "ap-northeast-1a"
 }
 
 
