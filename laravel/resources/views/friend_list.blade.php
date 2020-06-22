f5ebf4f69255affbe66b9862b6144fdf94030721
nanami <88naechan88@gmail.com>
2020-06-22 17:18:36 +0900

    S3のリポジトリ名の変更

--- laravel/resources/views/friend_list.blade.php
+++ laravel/resources/views/friend_list.blade.php
@@ -7,7 +7,7 @@
             @if(count($friends) > 0)
             @foreach($friends as $friend)
             @if(!$friend->user->user_image==null)
-            <img src="https://s3-ap-northeast-1.amazonaws.com/aws.fish-records/{{$friend->user->user_image}}">
+            <img src="https://s3-ap-northeast-1.amazonaws.com/tsurins-images/{{$friend->user->user_image}}">
             @endif
             <a href="{{route('friend_profile',$friend->friend_id)}}">{{$friend->user->name}}</a>
             {{$friend->user->description}}<br/>
